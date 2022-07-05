00d524b8783085fb3446a6fb20893e3f5c46e361
Billy Buehl <billybuehl792@gmail.com>
2022-07-05 15:15:22 -0400

    Flask App 04: Static + Bootstrap + extends

--- app/admin_views.py
+++ app/admin_views.py
@@ -4,7 +4,7 @@ from flask import render_template
 
 @app.route("/admin/dashboard")
 def admin_dashboard():
-    return render_template("admin/dashboard.html")
+    return render_template("admin/dashboard.html", title="Admin Dashboard")
 
 @app.route("/admin/profile")
 def admin_profile():
