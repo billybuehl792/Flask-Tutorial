4511a3853ece214f8b05e21d7115b1ac4f0d7c84
Billy Buehl <billybuehl792@gmail.com>
2022-07-21 10:38:45 -0400

    Flask App 07: Forms + requests

--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 
 from app import app
-from flask import render_template
+from flask import render_template, request, redirect
 from datetime import datetime
 
 @app.template_filter('clean_date')
@@ -60,3 +60,17 @@ def jinja():
 @app.route("/about")
 def about():
     return render_template("public/about.html")
+
+@app.route("/sign-up", methods=["GET", "POST"])
+def sign_up():
+
+    if request.method == "POST":
+        req = request.form
+        username = req.get("username")
+        email = req.get("email")
+        password = req.get("password")
+
+        print(f"user: {username}, email: {email}, password: {password}")
+        return redirect(request.url)
+    
+    return render_template("public/sign_up.html")
